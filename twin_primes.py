24e3a979f8688be67286816061f217048be354f5
Mahmoud <mahmoud.ahmed.ibrahim@outlook.com>
2018-10-14 15:41:24 +0200

    add test files

--- twin_primes.py
+++ twin_primes.py
@@ -1,34 +1,45 @@
 import tweepy
 
-def parse_primes(file):
-    file = open(file, "r")
-    data_file = file.read()
-    data = []
-    curStr = ''
-    for c in data_file:
-        if c.isdigit():
-            curStr = curStr + c
-        elif curStr != '':
-            data.append(curStr)
-            curStr = ''
-        else:
-            curStr = ''
+# # def parse_primes(file):
+# #     file = open(file, "r")
+# #     data_file = file.read()
+# #     data = []
+# #     curStr = ''
+# #     for c in data_file:
+# #         if c.isdigit():
+# #             curStr = curStr + c
+# #         elif curStr != '':
+# #             data.append(curStr)
+# #             curStr = ''
+# #         else:
+# #             curStr = ''
     
-    return ','.join(data)
+# #     return ','.join(data)
 
-data = parse_primes("twin_primes.txt")
+# # data = parse_primes("twin_primes.txt")
 
-CONSUMER_KEY = 'xW5vBk2vBPZLBG4cL4fkaehk2'
-CONSUMER_SECRET = 'QlCKicmXXwTpSGOXOChnN1vARBvOQUEwpmL9NSBzuEWfDRC5AT '
-ACCESS_TOKEN = '1050371798490120192-cpDgjuUuR8CCRkYO6O1TePcGKtsMls'
-ACCESS_TOKEN_SECRET = 'zH8zoYuogIkCC4BxAdV7DQ5ej0JkO9PKJZrrWjn6iwywU'
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+consumer_token = 'g263nvk52IG47YxaeXCgUIL9H'
+consumer_secret = '9FWw8U8aNwB0g9xklAIiPzSSbmeR5RZkqTiH0fOXyRHGYP7zAt'
+ACCESS_TOKEN = '1050371798490120192-FPQAZiOLVrtyeLaVczxmPgr8ynOg67'
+ACCESS_TOKEN_SECRET = 'rnPG31LhRVolMaLWRJGSIReQWfKnEPTArqIZGNkzioXA6'
+# auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+# auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# api = tweepy.API(auth)
+
+# # last_tweet = api.user_timeline(screen_name = 'quote4mee',count=1)[0].text
+# # last_posted_elem = last_tweet.split(',')[0]
+# # next_index = data.index(last_posted_elem) + 1
+# # twit = data[next_index] + ', ' + str(int(data[next_index]) + 2)
+# twit = "Hello I`m Mahmoud"
+# api.update_status(twit)
+
+auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
+try:
+    redirect_url = auth.get_authorization_url()
+except tweepy.TweepError:
+    print ('Error! Failed to get request token.')
+
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
 
-# last_tweet = api.user_timeline(screen_name = 'quote4mee',count=1)[0].text
-# last_posted_elem = last_tweet.split(',')[0]
-# next_index = data.index(last_posted_elem) + 1
-# twit = data[next_index] + ', ' + str(int(data[next_index]) + 2)
-twit = "Hello I`m Mahmoud"
-api.update_status(twit)
+api = tweepy.API(auth)
+api.update_status('tweepy + oauth!')
